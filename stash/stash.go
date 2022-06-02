a1929307b97727001827393fd633679bd947bd2a
zitrator <zitrator@yahoo.com>
2022-06-02 19:48:07 +0300

    build

--- stash/stash.go
+++ stash/stash.go
@@ -1,13 +1,23 @@
 package stash
 
+// todo: transaction log
+// todo: encrypt data on disk
+
 import (
 	"sync"
 )
 
 // Stash in-memory storage
-// todo: transaction log
-// todo: encrypt data on disk
 type Stash struct {
 	sync.RWMutex
-	m map[string]string
+	m map[string]interface{}
+}
+
+func (s *Stash) Put(key string, doc interface{}) error {
+	s.m[key] = doc
+	return nil
+}
+
+func (s *Stash) Get(key string) (interface{}, error) {
+	return s.m[key], nil
 }
